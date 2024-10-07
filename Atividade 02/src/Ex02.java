8a5a90f0bc9a9487f9d45c10a1f9b58f2114eee3
Gabs <bieldds05@gmail.com>
2024-10-07 15:21:33 -0500

    Resolução da Questão c)

--- Atividade 02/src/Ex02.java
+++ Atividade 02/src/Ex02.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Ex02 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Abre o scanner
         System.out.println("Digite a 1ª Variável: ");
-        Double valor1 = scanner.nextDouble();
+        Double valor1 = scanner.nextDouble(); // Leitura da 1ª vaiável
         System.out.println("Agora, digite a 2ª Variável: ");
-        Double valor2 = scanner.nextDouble();
-        System.out.println("Soma: " + (valor1+valor2));
-        System.out.println("Subtração: " + (valor1-valor2));
-        System.out.println("Multiplicação: " + (valor1*valor2));
-        System.out.println("Divisão: " + (valor1/valor2));
+        Double valor2 = scanner.nextDouble(); // Leitura da 2ª Variável
+        System.out.println("Soma: " + (valor1+valor2)); // Soma de Variáevis
+        System.out.println("Subtração: " + (valor1-valor2)); // Subtração de variáveis
+        System.out.println("Multiplicação: " + (valor1*valor2)); // Multiplicação de Variáveis
+        System.out.println("Divisão: " + (valor1/valor2)); // Divisão de Variáveis
     }
 }
