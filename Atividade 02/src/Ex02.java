883b6893bec40c662672304022035aa33e4666dc
Gabs <bieldds05@gmail.com>
2024-10-07 15:19:44 -0500

    Resolução da Questão b)

--- Atividade 02/src/Ex02.java
+++ Atividade 02/src/Ex02.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 public class Ex02 {
     public static void main(String[] args) {
-        int inteiro1 = 5;
-        int inteiro2 = 7;
-        System.out.println("Soma: " + (inteiro1+inteiro2));
-        System.out.println("Subtração: " + (inteiro1-inteiro2));
-        System.out.println("Multiplicação: " + (inteiro1*inteiro2));
-        System.out.println("Divisão: " + (inteiro1/inteiro2));
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a 1ª Variável: ");
+        Double valor1 = scanner.nextDouble();
+        System.out.println("Agora, digite a 2ª Variável: ");
+        Double valor2 = scanner.nextDouble();
+        System.out.println("Soma: " + (valor1+valor2));
+        System.out.println("Subtração: " + (valor1-valor2));
+        System.out.println("Multiplicação: " + (valor1*valor2));
+        System.out.println("Divisão: " + (valor1/valor2));
     }
 }
