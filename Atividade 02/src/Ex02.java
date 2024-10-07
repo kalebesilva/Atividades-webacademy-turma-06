24fa22e046dddb7dd36bd8fa6e1779819bb96554
Gabs <bieldds05@gmail.com>
2024-10-07 15:24:55 -0500

    Resolução da Questão d)

--- Atividade 02/src/Ex02.java
+++ Atividade 02/src/Ex02.java
@@ -2,14 +2,35 @@ import java.util.Scanner;
 
 public class Ex02 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Abre o scanner
+        /**
+         * Abre o scanner:
+         */
+        Scanner scanner = new Scanner(System.in); 
         System.out.println("Digite a 1ª Variável: ");
-        Double valor1 = scanner.nextDouble(); // Leitura da 1ª vaiável
+        /**
+         * Leitura da 1ª variável
+         */
+        Double valor1 = scanner.nextDouble();
         System.out.println("Agora, digite a 2ª Variável: ");
-        Double valor2 = scanner.nextDouble(); // Leitura da 2ª Variável
-        System.out.println("Soma: " + (valor1+valor2)); // Soma de Variáevis
-        System.out.println("Subtração: " + (valor1-valor2)); // Subtração de variáveis
-        System.out.println("Multiplicação: " + (valor1*valor2)); // Multiplicação de Variáveis
-        System.out.println("Divisão: " + (valor1/valor2)); // Divisão de Variáveis
+        /**
+         * Leitura da 2ª Variável
+         */
+        Double valor2 = scanner.nextDouble();
+        /**
+         * Soma de Variáevis
+         */
+        System.out.println("Soma: " + (valor1+valor2));
+        /**
+         * Subtração de variáveis
+         */
+        System.out.println("Subtração: " + (valor1-valor2));
+        /**
+         * Multiplicação de Variáveis
+         */
+        System.out.println("Multiplicação: " + (valor1*valor2));
+        /**
+         * Divisão de Variáveis
+         */
+        System.out.println("Divisão: " + (valor1/valor2));
     }
 }
