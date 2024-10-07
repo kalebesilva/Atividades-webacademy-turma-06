439d5ce4a65e16195b316c217d4c02a22a2ba73a
Gabs <bieldds05@gmail.com>
2024-10-07 15:12:17 -0500

    Resolução da Questão a)

---
+++ Atividade 02/src/Ex02.java
@@ -0,0 +1,11 @@
+public class Ex02 {
+    public static void main(String[] args) {
+        int inteiro1 = 5;
+        int inteiro2 = 7;
+        System.out.println("Soma: " + (inteiro1+inteiro2));
+        System.out.println("Subtração: " + (inteiro1-inteiro2));
+        System.out.println("Multiplicação: " + (inteiro1*inteiro2));
+        System.out.println("Divisão: " + (inteiro1/inteiro2));
+        
+    }
+}
