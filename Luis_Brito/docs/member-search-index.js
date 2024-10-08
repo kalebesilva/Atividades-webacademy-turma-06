fe57dc5a460579918f2e97808ae66e6bdea09256
LuisH-Brito <luishsbb1570@gmail.com>
2024-10-08 17:30:58 -0500

    feat: atividade 2 - Luís Brito

---
+++ Luis_Brito/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"QuatroOperacoes","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"QuatroOperacoes","l":"QuatroOperacoes()","u":"%3Cinit%3E()"}];updateSearchResults();
